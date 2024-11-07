5d00691a9df46c14b6d3991f7d04e92b636c7613
phn00dev <hudayberdipolat@gmail.com>
2024-11-07 13:54:26 +0500

    user handler ready

--- internal/domain/user/handler/user_handler_imp.go
+++ internal/domain/user/handler/user_handler_imp.go
@@ -1,9 +1,14 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 
+	"github.com/phn00dev/go-crud/internal/domain/user/dto"
 	"github.com/phn00dev/go-crud/internal/domain/user/service"
+	bindandvalidate "github.com/phn00dev/go-crud/internal/utils/bind_and_validate"
+	"github.com/phn00dev/go-crud/internal/utils/response"
 
 )
 
@@ -18,17 +23,77 @@ func NewUserHandler(service service.UserService) UserHandler {
 }
 
 func (userHandler userHandlerImp) GetUser(ctx *gin.Context) {
-	panic("user handler imp")
+	userId, exists := ctx.Get("id")
+	if !exists {
+		response.Error(ctx, http.StatusBadRequest, "User ID is required", "User ID is required")
+		return
+	}
+	username, exists := ctx.Get("username")
+	if !exists {
+		response.Error(ctx, http.StatusBadRequest, "Username is required", "User ID is required")
+		return
+	}
+	user, err := userHandler.userService.GetUser(userId.(int), username.(string))
+	if err != nil {
+		response.Error(ctx, http.StatusInternalServerError, "Error fetching user", err.Error())
+		return
+	}
+	response.Success(ctx, http.StatusOK, "User data", user)
 }
 
 func (userHandler userHandlerImp) Update(ctx *gin.Context) {
-	panic("user handler imp")
+	userId, exists := ctx.Get("id")
+	if !exists {
+		response.Error(ctx, http.StatusBadRequest, "User ID is required", "User ID is required")
+		return
+	}
+
+	var updateRequest dto.UpdateUserRequest
+	if !bindandvalidate.BindAndValidateRequest(ctx, &updateRequest) {
+		return
+	}
+	// update user data
+	if err := userHandler.userService.Update(userId.(int), updateRequest); err != nil {
+		response.Error(ctx, http.StatusInternalServerError, "update error", err.Error())
+		return
+	}
+	response.Success(ctx, http.StatusOK, "update successfully", nil)
 }
 
 func (userHandler userHandlerImp) UpdatePassword(ctx *gin.Context) {
-	panic("user handler imp")
+	userId, exists := ctx.Get("id")
+	if !exists {
+		response.Error(ctx, http.StatusBadRequest, "User ID is required", "User ID is required")
+		return
+	}
+
+	var updatePasswordRequest dto.UpdateUserPasswordRequest
+	if !bindandvalidate.BindAndValidateRequest(ctx, &updatePasswordRequest) {
+		return
+	}
+	// update user data
+	if err := userHandler.userService.UpdatePassword(userId.(int), updatePasswordRequest); err != nil {
+		response.Error(ctx, http.StatusInternalServerError, "update password error", err.Error())
+		return
+	}
+	response.Success(ctx, http.StatusOK, "update  password successfully", nil)
 }
 
 func (userHandler userHandlerImp) Delete(ctx *gin.Context) {
-	panic("user handler imp")
+	userId, exists := ctx.Get("id")
+	if !exists {
+		response.Error(ctx, http.StatusBadRequest, "User ID is required", "User ID is required")
+		return
+	}
+	username, exists := ctx.Get("username")
+	if !exists {
+		response.Error(ctx, http.StatusBadRequest, "Username is required", "User ID is required")
+		return
+	}
+
+	if err := userHandler.userService.Delete(userId.(int), username.(string)); err != nil {
+		response.Error(ctx, http.StatusBadRequest, "delete error", err.Error())
+		return
+	}
+	response.Success(ctx, http.StatusOK, "delete successfully", nil)
 }
