448288f08ae7e54edeeb6105cde096eadb7cc0bf
phn00dev <hudayberdipolat@gmail.com>
2024-11-07 09:23:43 +0500

    ready httpClient and GenerateJwtToken function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/phn00dev/go-crud
 
 go 1.23.1
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
--- pkg/jwt_token/generate_token.go
+++ pkg/jwt_token/generate_token.go
@@ -1,1 +1,33 @@
 package jwttoken
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+
+)
+
+type Claims struct {
+	UserId   int    `json:"user_id"`
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+const SecretKey = "AFDAadwrwe&%&^DSDDSDI)(H:)!ss"
+
+func GenerateJwtToken(userId int, username string) (string, error) {
+	expirationTime := time.Now().Add(3 * time.Hour)
+	claims := &Claims{
+		UserId:   userId,
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(SecretKey))
+	if err != nil {
+		return "", nil
+	}
+	return tokenString, nil
+}
