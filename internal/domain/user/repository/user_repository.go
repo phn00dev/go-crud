8d8245fa7ccfd575ad217d50c7bfb2abf6626755
phn00dev <hudayberdipolat@gmail.com>
2024-11-07 12:36:32 +0500

    ready  user repository

--- internal/domain/user/repository/user_repository.go
+++ internal/domain/user/repository/user_repository.go
@@ -5,7 +5,7 @@ import "github.com/phn00dev/go-crud/internal/models"
 type UserRepository interface {
 	// user profile data
 	GetUser(userId int, username string) (*models.User, error)
-	Update(user models.User) error
-	UpdatePassword(password string) error
+	Update(userId int, user models.User) error
+	UpdatePassword(userId int, password string) error
 	Delete(userId int, username string) error
 }
--- internal/domain/user/repository/user_repository_imp.go
+++ internal/domain/user/repository/user_repository_imp.go
@@ -17,17 +17,21 @@ func NewUserRepository(db *gorm.DB) UserRepository {
 }
 
 func (userRepo userRepositoryImp) GetUser(userId int, username string) (*models.User, error) {
-	panic("user repo imp")
+	var user models.User
+	if err := userRepo.db.Where("id =? AND username =?", userId, username).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
 
-func (userRepo userRepositoryImp) Update(user models.User) error {
-	panic("user repo imp")
+func (userRepo userRepositoryImp) Update(userId int, user models.User) error {
+	return userRepo.db.Where("id=?", userId).Updates(&user).Error
 }
 
-func (userRepo userRepositoryImp) UpdatePassword(password string) error {
-	panic("user repo imp")
+func (userRepo userRepositoryImp) UpdatePassword(userId int, password string) error {
+	return userRepo.db.Where("id=?", userId).Update("password_hash", password).Error
 }
 
 func (userRepo userRepositoryImp) Delete(userId int, username string) error {
-	panic("user repo imp")
+	return userRepo.db.Where("id=? AND username=?", userId, username).Error
 }
