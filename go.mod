6982b3937cc498be5908d31365e1808588c6df50
phn00dev <hudayberdipolat@gmail.com>
2024-11-07 09:32:44 +0500

    ready config

--- go.mod
+++ go.mod
@@ -3,3 +3,11 @@ module github.com/phn00dev/go-crud
 go 1.23.1
 
 require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 
+	github.com/joho/godotenv v1.5.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,38 @@
 package config
+
+import "github.com/ilyakaznacheev/cleanenv"
+
+type Config struct {
+	DbConfig     dbConfig     `json:"db_config"`
+	HttpConfig   httpConfig   `json:"http_config"`
+	FolderConfig folderConfig `json:"folder_config"`
+}
+
+type dbConfig struct {
+	DbHost     string `json:"db_host" env:"DB_HOST"`
+	DbPort     string `json:"db_port" env:"DB_PORT"`
+	DbUser     string `json:"db_user" env:"DB_USER"`
+	DbPassword string `json:"db_password" env:"DB_PASSWORD"`
+	DbName     string `json:"db_name" env:"DB_NAME"`
+	DbSslMode  string `json:"db_sll_mode" env:"DB_SSL_MODE"`
+	DbTimeZone string `json:"db_time_zone" env:"DB_TIME_ZONE"`
+}
+type httpConfig struct {
+	HttpHost  string `json:"http_host" env:"HTTP_SERVER"`
+	HttpPort  string `json:"http_port" env:"HTTP_PORT"`
+	AppName   string `json:"app_name" env:"APP_NAME"`
+	AppHeader string `json:"app_header" env:"APP_HEADER"`
+}
+
+type folderConfig struct {
+	PublicPath string `json:"public_path" env:"PUBLIC_PATH"`
+	RootPath   string `json:"root_path" env:"ROOT_PATH"`
+}
+
+func GetConfig() (*Config, error) {
+	var cfg Config
+	if err := cleanenv.ReadConfig("../.env", &cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
