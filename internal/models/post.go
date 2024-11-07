fb1288dcdf4e2bce4b98c4cb73606c023cc445c1
phn00dev <hudayberdipolat@gmail.com>
2024-11-07 10:20:48 +0500

    post and user model ready

--- internal/models/post.go
+++ internal/models/post.go
@@ -1,1 +1,14 @@
 package models
+
+import "time"
+
+type Post struct {
+	Id        int       `json:"id"`
+	PostTitle string    `json:"post_title"`
+	PostSlug  string    `json:"post_slug"`
+	PostDesc  string    `json:"post_desc"`
+	PostImage string    `json:"post_image"`
+	ViewCount int       `json:"view_count"`
+	CreatedAt time.Time `json:"created_at"`
+	UserId    int       `json:"user_id"`
+}
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,10 @@
 package models
+
+import "time"
+
+type User struct {
+	Id           int       `json:"id"`
+	Username     string    `json:"username"`
+	PasswordHash string    `json:"password_hash"`
+	CreatedAt    time.Time `json:"created_at"`
+}
